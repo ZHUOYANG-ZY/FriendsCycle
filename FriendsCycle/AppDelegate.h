53ee7ca0c76ed1a3cdf9f977279ff9d5ca8a9f30
jinyueyang <1301723823@qq.com>
2017-07-12 13:16:38 +0800

    friendsCycle

---
+++ FriendsCycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FriendsCycle
+//
+//  Created by kede on 2017/7/12.
+//  Copyright © 2017年 kede. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FriendsCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FriendsCycle
+//
+//  Created by kede on 2017/7/12.
+//  Copyright © 2017年 kede. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
